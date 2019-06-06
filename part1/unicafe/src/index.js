e7564e0b3c926f93f23f84db4a6c79654989364b
Martin Almén <get_get_martin@hotmail.com>
2019-06-06 18:50:51 +0200

    step 1 for real

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+const Statistics = (props) => {
+    return (
+        <div>
+            <span>good {props.good}</span><br/>
+            <span>neutral {props.neutral}</span><br/>
+            <span>bad {props.bad}</span><br/>
+        </div>
+    )
+}
+
+const FeedbackButtons = (props) => {
+    return(
+        <div>
+            <button style={{margin: '5px'}} onClick={props.goodClickHandler}>good</button>
+            <button style={{margin: '5px'}} onClick={props.neutralClickHandler}>neutral</button>
+            <button style={{margin: '5px'}} onClick={props.badClickHandler}>bad</button>
+        </div>
+    )
+}
+
+const App = () => {
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+
+    const setGoodValue = val => {
+        setGood(val);
+    }
+
+    const setNeutralValue = val => {
+        setNeutral(val);
+    }
+
+    const setBadValue = val => {
+        setBad(val);
+    }
+
+    return(
+        <div>
+            <h1>Give Feedback</h1>
+            <FeedbackButtons goodClickHandler={() => setGoodValue(good + 1)} neutralClickHandler={() => setNeutralValue(neutral + 1)} badClickHandler={() => setBadValue(bad + 1)}/>
+            <h1>Statistics</h1>
+            <Statistics good={good} neutral={neutral} bad={bad}/>
+        </div>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
